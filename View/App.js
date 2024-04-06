51e78634267df8a007c57a36d3ee67c374010a21
aymane hilmi <aymanehilmi1@gmail.com>
2024-04-06 23:08:08 +0200

    maj Login Page

--- View/App.js
+++ View/App.js
@@ -2,7 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Navigation from './Navigation';
 import { LogIn } from 'react-native-feather';
-import LoginPage from './components/loginPage';
 
 export default function App() {
   return (
