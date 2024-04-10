4f03e7c977d4662e984742d91c918301ed2c9e45
clementfornes13 <clement.fornes@viacesi.fr>
2024-04-10 09:31:00 +0200

    yo

--- View/App.js
+++ View/App.js
@@ -1,11 +1,4 @@
 import Navigation from './Navigation';
-import { Provider } from 'react-redux'
-import { LogIn } from 'react-native-feather';
-
-
-
-
-
 
 export default function App() {
   return (
